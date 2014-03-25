5535c51d1906dab43f01b14a5bf2e19f837c2382
Manuel Riezebosch <mriezebosch@gmail.com>
2014-03-25 13:38:11 +0100

    slashes aangepast zodat het ook in cmd werkt

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-Tools/NuGet.exe restore Glitter.sln
-'C:/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe' Glitter/Glitter.csproj
+Tools\\NuGet.exe restore Glitter.sln
+C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\MSBuild.exe Glitter/Glitter.csproj
 start Glitter/bin/Debug/Glitter.exe
